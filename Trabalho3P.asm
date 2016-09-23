b5b3843f28f58e38663e61df798ee36bd60357a3
Pedro Yan Ornelas <pedroyane@gmail.com>
2016-09-23 10:02:36 -0300

    Realiza a leitura serial da imagem .raw

--- Trabalho3P.asm
+++ Trabalho3P.asm
@@ -1,6 +1,6 @@
 .data
 
-memory_buffer:		.space 1228
+memory_buffer:		.space 4
 tamnho_Imagem:		.word 12288
 nomeArquivo:		.asciiz "lenaeye.raw"
 
@@ -15,115 +15,29 @@ loadImg:
 	
 	#carrega pro buffer
 	move $t2,$v0
-	li $t0,0
+	addi $t0,$zero,0x10040000 
 	li $t1,0
 	la $t3,memory_buffer
 	jal LoopBuffer
 	
-	addi $t0,$zero,0
-	la $t1,memory_buffer 
-	add $t2, $zero, 0x10040000
-	add $t3, $zero, $a2
-	j loopLeitura
 
 LoopBuffer:
-	#t0: condicaoParada
-	#t1: i
+	#t0: Area da Heap
+	#t1: variavelTemporaria
 	#t2: fileDescriptor
 	#t3: memoryBuffer
+	#v0: Retorno da função fread. se 0 = end of file
 	li $v0, 14
+	move $a0, $t2
 	move $a1,$t3
-	addi $a2, $zero, 4
+	addi $a2, $zero, 3
 	syscall
-
-loopLeitura:
-	# t0: i
-	# t1: Ponteiro da memoria do buffer que contém a imagem Lida
-	# t2: Regiao de memoria da heap (onde vão ser armazenados os bytes)
-	# t3: Condição de parada do loop
-	# t4: Byte lido
-	beq $t0,$t3, exit
+	beq $v0, $zero, exit
+	lw $t1, 0($t3)
+	sw $t1, 0($t0)
+	addi $t0,$t0,4
+	j LoopBuffer
 	
-	#zero1
-	sb $zero, 3($t2)
-	
-	#R1
-	lb $t4, 3($t1)
-	sb $t4, 2($t2)
-	
-	#G1
-	lb $t4, 2($t1)
-	sb $t4, 1($t2)
-	
-	#B1
-	lb $t4, 1($t1)
-	sb $t4, 0($t2)
-	
-	lw $t4, 0($t2)
-	sw $t4, 0($t2)
-	addi $t2, $t2, 4 ## passa uma word
-	addi $t0, $t0, 3 ## Leu 1 RGB -> i+3
-	sb $zero, 3($t2) # zero2
-	
-	#R2
-	lb $t4, 0($t1)
-	sb $t4, 2($t2)
-	
-	#G2
-	addi $t1, $t1, 4 ##next word -> imagem lida
-	lb $t4, 3($t1)
-	sb $t4, 1($t2)
-	
-	#B2
-	lb $t4, 2($t1)
-	sb $t4, 0($t2)
-	
-	#zero3
-	lw $t4, 0($t2)
-	sw $t4, 0($t2)
-	addi $t2, $t2, 4 ## passa uma word
-	addi $t0, $t0, 3 ## Leu 1 RGB -> i+3
-	sb $zero, 3($t2)
-	
-	#R3
-	lb $t4, 1($t1)
-	sb $t4, 2($t2)
-	
-	#G3
-	lb $t4, 0($t1)
-	sb $t4, 1($t2)
-	
-	#B3
-	addi $t1, $t1, 4 ##next word -> imagem lida
-	lb $t4, 3($t1)
-	sb $t4, 0($t2)
-	
-	#zero4
-	lw $t4, 0($t2)
-	sw $t4, 0($t2)
-	addi $t2, $t2, 4 ## passa uma word
-	addi $t0, $t0, 3 ## Leu 1 RGB -> i+3
-	sb $zero, 3($t2)
-	
-	#R4
-	lb $t4, 2($t1)
-	sb $t4, 2($t2)
-	
-	#G4
-	lb $t4, 1($t1)
-	sb $t4, 1($t2)
-	
-	#B4
-	lb $t4, 0($t1)
-	sb $t4, 0($t2)
-	
-	lw $t4, 0($t2)
-	sw $t4, 0($t2)
-	addi $t2, $t2, 4 ## passa uma word
-	addi $t0, $t0, 3 ## Leu 1 RGB -> i+3
-	addi $t1, $t1, 4 ##next word -> imagem lida
-	
-	j loopLeitura
 exit:
 	li $v0,10
 	syscall
